84e52c14d7094ce140405932177570d06f344534
Cory Martin <cory.martin@dozensoft.com>
2019-09-20 20:34:15 -0500

    add custom attributes to test admin sign up

--- cicd-destroy.sh
+++ cicd-destroy.sh
@@ -3,4 +3,5 @@ tail cdkdeployresult_cognito.txt
 cd cdk
 echo "y" | cdk destroy ReinventCatalogTestingCognito
 echo "y" | cdk destroy ReinventCatalogTestingDynamoDb
-echo "y" | cdk destroy ReinventCatalogTestingS3
+# cdk doesn't actually delete the table >:(
+aws dynamodb delete-table --table-name ReinventCatalogTesting
